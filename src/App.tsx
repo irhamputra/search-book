eff69da503ed864556d2f07612b60a432d479037
muhamad <muhamad@future.rent>
2019-12-24 07:33:38 +0100

    final code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback } from 'react';
+import useBookSearch from './useBookSearch';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [query, setQuery] = useState('');
+    const [pageNumber, setPageNumber] = useState(1);
+    const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
+    const ref = useRef(null);
+    const lastBookElement = useCallback(
+        node => {
+            if (loading) return;
+            if (ref.current) {
+                // @ts-ignore because of null
+                ref.current.disconnect();
+            }
+            // @ts-ignore because of null
+            ref.current = new IntersectionObserver(entries => {
+                if (entries[0].isIntersecting && hasMore) {
+                    setPageNumber(prevState => prevState + 1);
+                }
+            });
+            if (node) {
+                // @ts-ignore because of null
+                ref.current.observe(node);
+            }
+        },
+        [loading, hasMore]
+    );
+
+    const handleSearch = (e: any) => {
+        setQuery(e.target.value);
+        setPageNumber(1);
+    };
+
+    return (
+        <div className='App'>
+            <input type='text' value={query} onChange={handleSearch} />
+            <ul>
+                {books.map((book: any, i) => {
+                    if (book.isbn) {
+                        if (books.length === i + 1) {
+                            return (
+                                <li ref={lastBookElement} key={i}>
+                                    {book.title} -{' '}
+                                    <strong>{book.isbn[0]}</strong>
+                                </li>
+                            );
+                        } else {
+                            return (
+                                <li key={i}>
+                                    {book.title} -{' '}
+                                    <strong>{book.isbn[0]}</strong>
+                                </li>
+                            );
+                        }
+                    }
+                })}
+            </ul>
+
+            <h1>{loading && 'Loading...'}</h1>
+            <h3>{error && 'Error!!'}</h3>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/useBookSearch.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useBookSearch = (query: string, pageNumber: number) => {
+    const [loading, setLoading] = useState(true);
+    const [books, setBooks] = useState([]);
+    const [error, setError] = useState(false);
+    const [hasMore, setHasMore] = useState(false);
+
+    useEffect(() => {
+        setBooks([]);
+    }, [query]);
+
+    useEffect(() => {
+        setLoading(true);
+        setError(false);
+        let cancel: () => void;
+        axios
+            .get('http://openlibrary.org/search.json', {
+                params: { q: query, page: pageNumber },
+                cancelToken: new axios.CancelToken(c => (cancel = c))
+            })
+            .then(res => {
+                setBooks((prevState): any => {
+                    return [
+                        ...new Set<any[]>([...prevState, ...res.data.docs])
+                    ];
+                });
+                setHasMore(res.data.docs.length > 0);
+                console.log(res.data);
+                setLoading(false);
+            })
+            .catch(e => {
+                if (axios.isCancel(e)) return;
+                setError(true);
+            });
+        return () => cancel();
+    }, [query, pageNumber]);
+
+    return { loading, error, books, hasMore };
+};
+
+export default useBookSearch;
